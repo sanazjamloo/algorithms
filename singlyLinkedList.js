32a72e2f382e94411eb0cd88a9928c8afcee912e
sanazjamloo <sanaz.jamloo@gmail.com>
2019-02-22 20:29:03 -0800

    add singly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -116,3 +116,126 @@ removeFrom(index)
         return curr.element;
     }
 }
+
+removeElement(element)
+{
+    var current = this.head;
+    var prev = null;
+
+    // iterate over the list
+    while (current != null) {
+        // comparing element with current
+        // element if found then remove the
+        // and return true
+        if (current.element == = element) {
+            if (prev == null) {
+                this.head = current.next;
+            } else {
+                prev.next = current.next;
+            }
+            this.size--;
+            return current.element;
+        }
+        prev = current;
+        current = current.next;
+    }
+    return -1;
+}
+
+// finds the index of element
+indexOf(element)
+{
+    var count = 0;
+    var current = this.head;
+
+    // iterae over the list
+    while (current != null) {
+        // compare each element of the list
+        // with given element
+        if (current.element == = element)
+            return count;
+        count++;
+        current = current.next;
+    }
+
+    // not found
+    return -1;
+}
+
+// checks the list for empty
+isEmpty()
+{
+    return this.size == 0;
+}
+
+// gives the size of the list
+size_of_list()
+{
+    console.log(this.size);
+}
+
+// prints the list items
+printList()
+{
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+        str += curr.element + " ";
+        curr = curr.next;
+    }
+    console.log(str);
+}
+
+
+
+/***Implementation and Testing ***/
+
+// creating an object for the
+// Linkedlist class
+var ll = new LinkedList();
+
+// testing isEmpty on an empty list
+// returns true
+console.log(ll.isEmpty());
+
+// adding element to the list
+ll.add(10);
+
+// prints 10
+ll.printList();
+
+// returns 1
+console.log(ll.size_of_list());
+
+// adding more elements to the list
+ll.add(20);
+ll.add(30);
+ll.add(40);
+ll.add(50);
+
+// returns 10 20 30 40 50
+ll.printList();
+
+// prints 50 from the list
+console.log("is element removed ?" + ll.removeElement(50));
+
+// prints 10 20 30 40
+ll.printList();
+
+// returns 3
+console.log("Index of 40 " + ll.indexOf(40));
+
+// insert 60 at second positon
+// ll contains 10 20 60 30 40
+ll.insertAt(60, 2);
+
+ll.printList();
+
+// returns false
+console.log("is List Empty ? " + ll.isEmpty());
+
+// remove 3rd element from the list
+console.log(ll.removeFrom(3));
+
+// prints 10 20 60 40
+ll.printList();
